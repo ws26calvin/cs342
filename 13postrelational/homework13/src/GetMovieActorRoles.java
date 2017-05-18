4228487d653fb5f6fcf3952e55add16f885481e5
ws26calvin <ws26@students.calvin.edu>
2017-05-18 12:15:19 -0400

    Update GetMovieActorRoles.java

--- 13postrelational/homework13/src/GetMovieActorRoles.java
+++ 13postrelational/homework13/src/GetMovieActorRoles.java
@@ -1,5 +1,19 @@
-/**
- * Created by ws26 on 5/17/2017.
- */
+import oracle.kv.*;
+
+import java.util.Arrays;
+import java.util.Map;
+
 public class GetMovieActorRoles {
+
+    public void gmar(String actorID, String movieID) {
+        KVStore store = KVStoreFactory.getStore(new KVStoreConfig("kvstore", "localhost:5000"));
+
+        Key roleMajorKeyPathOnly = Key.createKey(Arrays.asList("role", actorID, movieID));
+        Map<Key, ValueVersion> fields = store.multiGet(roleMajorKeyPathOnly, null, null);
+        for (Map.Entry<Key, ValueVersion> field : fields.entrySet()) {
+            String fieldValue = new String(field.getValue().getValue().getValue());
+            System.out.println(fieldValue);
+        }
+        store.close();
+    }
 }
