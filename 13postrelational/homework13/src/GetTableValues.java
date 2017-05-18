62d6ecc2e8304ae5849475d5177b9f0d360c6f08
ws26calvin <ws26@students.calvin.edu>
2017-05-18 12:14:35 -0400

    Update GetTableValues.java

--- 13postrelational/homework13/src/GetTableValues.java
+++ 13postrelational/homework13/src/GetTableValues.java
@@ -1,5 +1,17 @@
-/**
- * Created by ws26 on 5/17/2017.
- */
+import oracle.kv.*;
+
+import java.util.Arrays;
+import java.util.Map;
+
 public class GetTableValues {
+    public void gtv(String id) {
+        KVStore store = KVStoreFactory.getStore(new KVStoreConfig("kvstore", "localhost:5000"));
+        Key majorKeyPathOnly = Key.createKey(Arrays.asList("movie", id));
+        Map<Key, ValueVersion> fields = store.multiGet(majorKeyPathOnly, null, null);
+        for (Map.Entry<Key, ValueVersion> field : fields.entrySet()) {
+            String fieldValue = new String(field.getValue().getValue().getValue());
+            System.out.println(fieldValue);
+        }
+        store.close();
+    }
 }
